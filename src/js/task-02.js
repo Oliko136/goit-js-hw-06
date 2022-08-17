3761b58c5fa5834c7726bbe95ac79f14e85702c9
Oliko136 <olchick136@gmail.com>
2022-08-17 17:55:34 +0400

    Task 2 completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const element = document.createElement('li');
+  element.textContent = ingredient;
+  element.classList.add('item');
+  return element;
+})
+
+ingredientsEl.append(...elements);
+
+
