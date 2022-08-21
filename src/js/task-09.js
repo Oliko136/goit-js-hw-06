eee93b2389930f3ae7ac467ed745f899d8d79e4c
Oliko136 <olchick136@gmail.com>
2022-08-21 16:12:39 +0400

    Task 9 completed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorName = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', onColorChange);
+
+function onColorChange(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
+}
