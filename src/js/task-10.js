2bf62bee098bbc1d717216c74500408c3ec618d3
Oliko136 <olchick136@gmail.com>
2022-08-22 13:35:20 +0400

    Task 10 completed

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const numberInput = document.querySelector('#controls > input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxContainer = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const boxes = [];
+  amount = numberInput.value;
+
+  for (let i = 0; i < amount; i += 1){
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+
+    boxes.push(box);
+  }
+
+  boxContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxContainer.innerHTML = '';
+  numberInput.value = '';
+}
+
+
